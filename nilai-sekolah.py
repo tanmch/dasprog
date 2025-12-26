ee8c267fb250dfa190b1063973c0979097bea1b6
Michael Christian Handoko <suttanchristian@apps.ipb.ac.id>
2025-12-26 08:50:20 +0700

    Input Nilai Sekolah Sederhana

---
+++ nilai-sekolah.py
@@ -0,0 +1,36 @@
+print('nilai sekolah')
+
+Nilai = int(input('Masukkan Nilai :'))
+
+if (Nilai >= 70 <= 100):
+    print('selamat')
+    print('lulus')
+
+elif (Nilai > 101):
+    print('Maaf, Nilai Yang Dimasukkan Tidak Sesuai. Coba Lagi')
+else:
+    print('Maaf, Coba Lagi Di Hari Lain')
+
+
+
+# dalam if, true dan false tidak boleh huruf kecil, harus besar
+# else dan elif hampir sama, elif = terpisah dari if, else if.
+
+# if else bertingkat
+
+# buat program untuk menentukan apakah seseorang boleh jika 21 tahun atau lebih, diperbolehkan menikah
+# else, maaf anda belum cukup umur
+
+# yang kedua, input himpunan 4 angka dimana 4 angka tsb jika angka pertama lebih besar dari bilangan 2,3,4
+# outputnya satu bilangan
+# jika 4 angka sama, outputny 4 angka sama. 
+
+# input 2 var. satu nilai, satu usia.
+# jika nilainya >= 80 dan umur >= 20, outputnya selamat ya kak, kamu lulus
+# jika nilai <= 80, maaf ya kak, coba lagi
+# jika umur <= 20 dan nilai >= 80, outputnya selamat ya dek, kamu lulus.
+# jika nilai <= 80 dan umur <= 20, maaf ya dek coba lagi
+# output pesannya bebas        
+
+# source codeny ditulis soal 1, soal 2, soal 3
+# dalam bentuk screenshot
